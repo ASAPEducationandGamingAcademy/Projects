46f1b1746a5c66134d972e1f9e73954cc785453c
ASAP Education and Gaming Academy <69491021+ASAPEducationandGamingAcademy@users.noreply.github.com>
2020-08-30 12:59:19 -0400

    Create comments_and_variables.py

---
+++ Python for Beginners/comments_and_variables.py
@@ -0,0 +1,12 @@
+# Variables and Comments
+
+print("Hello World")
+# print("Hello World")
+
+# Variables
+main_character_name = "John"
+print(main_character_name + " is 25.")
+# print(main_character_name, "is 25.")
+print(main_character_name + " is very kind to his family.")
+print(main_character_name + " is very helpful to his neighbors.")
+print(main_character_name + " is very smart at math.")
