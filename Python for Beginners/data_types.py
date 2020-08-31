2e01648315a168d2732fdc8f424d3ea3a973e339
ASAP Education and Gaming Academy <69491021+ASAPEducationandGamingAcademy@users.noreply.github.com>
2020-08-31 09:30:42 -0400

    Create data_types.py

---
+++ Python for Beginners/data_types.py
@@ -0,0 +1,25 @@
+# Data Types
+
+# print("") # Strings
+name = "1234"
+print(name)
+
+# Integer
+# Any whole number negative or positive
+age = 20
+print(age)
+
+result = age + int(name)
+print(result)
+
+# Floats
+# Decimal number -> floating decimal point
+gpa = 3.9
+result = gpa + age
+print(result)
+
+# Boolean
+# true or false value
+
+is_male = True
+print(is_male)
