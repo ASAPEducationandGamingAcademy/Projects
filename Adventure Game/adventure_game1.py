6755c88cca3c9e12828362663fa5cf7b23795a95
ASAP Education and Gaming Academy <69491021+ASAPEducationandGamingAcademy@users.noreply.github.com>
2020-08-31 16:26:44 -0400

    Create adventure_game1.py

---
+++ Adventure Game/adventure_game1.py
@@ -0,0 +1,12 @@
+print("This is my first adventure game in python. Enjoy!")
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+print("Hello", name, "you are", age, "years old")
+# Depends on the text editor that you are using, the commas won't work.
+
+if age > 5:
+    print("You can play the game!")
+else:
+    print("You cannot play this game!")
+
+
